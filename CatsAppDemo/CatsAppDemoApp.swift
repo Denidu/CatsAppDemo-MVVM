0fc4d2b98a34c633c8f1c46aa30920fe9cd04951
Denidu Gamage <denidugamage21@gmail.com>
2024-10-24 09:09:26 +0530

    Initial Commit

---
+++ CatsAppDemo/CatsAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatsAppDemoApp.swift
+//  CatsAppDemo
+//
+//  Created by Denidu Gamage on 2024-10-24.
+//
+
+import SwiftUI
+
+@main
+struct CatsAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
